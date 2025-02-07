894c19cf774ee0daa3350820ad3f73c394930d37
Mercy Cherotich <cherotich@localhost.localdomain>
2025-02-07 15:43:10 +0300

    changes

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div>
+            <p>BEAUTY SALON</p>
+        </div>
+      <ul>
+        <li>Home</li>
+        <li>services</li>
+        <li>About Us</li>
+        <li>Gallery</li>
+        <li>Blogs</li>
+        <li>Testimonials</li>
+        <li>contact us</li>
+      </ul>
+
+    </div>
+  )
+}
+
+export default Navbar

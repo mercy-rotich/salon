74f21dd9ad48bf71477f95804bbd13166a4bb5c1
Mercy Cherotich <cherotich@localhost.localdomain>
2025-02-21 03:32:48 +0300

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import'./Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
+
+      <div className="logo">
+        MERCY <span>SALON</span>
+      </div>
+
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+         <li>Blogs</li>
+         <li>Gallery</li>
+        <li>contact</li>
+
+      </ul>
       
     </div>
   )

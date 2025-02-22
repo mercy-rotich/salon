aaa40708bd8d94abff1b9b60a5d068bb1a0ec723
Mercy Cherotich <cherotich@localhost.localdomain>
2025-02-23 02:18:56 +0300

    changes

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Hero.css'
+import hero_image1 from '../../assets/hero-image1.jpg'
 
 const Hero = () => {
   return (
-    <div>
-      
+    <div className='hero'>
+       <h3>Where tranquility meets transformation</h3>
+       <p>Whe showcase your <span>Skin</span>,<span>hair</span> and </p>
+       <p><span>
+       body</span> the care and attention </p>
+       <p>they deserve</p>
+
+       <button>Book now</button>
+
+       <img src={hero_image1} alt="" />
     </div>
   )
 }

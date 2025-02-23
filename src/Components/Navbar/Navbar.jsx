3a74ce4a044a84e2cf5885197846cc04ef5710c2
Mercy Cherotich <cherotich@localhost.localdomain>
2025-02-24 02:30:20 +0300

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,23 +5,25 @@ import logo from '../../assets/icon.jpg'
 
 const Navbar = () => {
   return (
-    <div className='navbar'>
+    <div className="container">
+      <div className='navbar'>
 
-      <div className="logo">
-        MERCY <img src={logo} alt="" /> <span>SALON</span>
-      </div>
+<div className="logo">
+  MERCY <img src={logo} alt="" /> <span>SALON</span>
+</div>
 
-      <ul>
-        <li>Home</li>
-        <li>Services</li>
-         <li>Blogs</li>
-         <li>Gallery</li>
-        <li>contact</li>
+<ul>
+  <li>Home</li>
+  <li>Services</li>
+   <li>Blogs</li>
+   <li>Gallery</li>
+  <li>contact</li>
 
-      </ul>
-      <img src={search_icon} alt="" className='search-icon'/>
-      <button>Login</button>
-      
+</ul>
+<img src={search_icon} alt="" className='search-icon'/>
+<button>Login</button>
+
+</div>
     </div>
   )
 }

387c99ca2815ffaee1446a7bcfb59b010a4ec188
Mercy Cherotich <cherotichm182@gmail.com>
2025-03-04 18:54:22 +0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react' 
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navbar from './Components/Navbar/Navbar'
 import Hero from './Components/Hero/Hero'
+import Login from './Components/LoginSignup/Login'
 
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
       <Navbar/>
       <Hero/>
+      <Routes>
+        <Route path ='login' element ={<Login/>}/>
+      </Routes>
+      </BrowserRouter>
       
     </div>
   )
---
+++ src/Components/LoginSignup/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+const Login = () => {
+  return (
+    <div className='container'> 
+      <div className="login">
+        <h1>Login</h1>
+      </div>
+      
+    </div>
+  )
+}
+
+export default Login
